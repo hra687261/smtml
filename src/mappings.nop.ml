c0e867fe66c0e9ddac3496b99c2fde3b567ad8f6
hra687261 <hra687261@gmail.com>
2024-09-18 19:50:34 +0200

    Add linebreak in missing solver error

--- src/mappings.nop.ml
+++ src/mappings.nop.ml
@@ -319,7 +319,7 @@ module Nop = struct
          solver (have a look at the supported solvers here: \
          https://github.com/formalsec/smtml?tab=readme-ov-file#supported-solvers). \
          Note that installing the solver with your system package manager is \
-         not enough, you must install it through opam."
+         not enough, you must install it through opam.@."
         solver_name solver_package;
       exit 1
 
